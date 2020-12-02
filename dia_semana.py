09fca3ee291af30e4731b70a2d3f87852a16350a
angelbosq <71437915+angelbosq@users.noreply.github.com>
2020-12-02 20:39:33 +0100

    Add files via upload

---
+++ dia_semana.py
@@ -0,0 +1,8 @@
+def dia_semana():
+    if dia_semana >= 1 and <= 7:
+        dias = ("lunes", "martes", "miercoles", "jueves", "viernes", "sabado", "domingo")
+        return dia
+    else:
+        return "Valor no valido"
+
+dia_semana()
